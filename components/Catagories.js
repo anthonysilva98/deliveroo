05791aba3886448c1ab9020854c6730de903dc73
anthonysilva98 <anthonyssilva98@gmail.com>
2023-02-15 19:58:01 -0800

    Catagory container

---
+++ components/Catagories.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Catagories = () => {
+  return (
+    <View>
+      <Text>C</Text>
+    </View>
+  );
+};
+
+export default Catagories;
