77e082bf485c26c4d07be38c6221d9c8ac605871
anthonysilva98 <anthonyssilva98@gmail.com>
2023-02-19 11:11:59 -0800

    Catagory Card with Media to display on scrollview

--- components/Catagories.js
+++ components/Catagories.js
@@ -1,8 +1,43 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import CategoryCard from "./CategoryCard";
 
 const Catagories = () => {
-  return <ScrollView>{/* Catagory Card */}</ScrollView>;
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+    >
+      {/* Catagory Card */}
+
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+        title="Testing"
+      />
+    </ScrollView>
+  );
 };
 
 export default Catagories;
