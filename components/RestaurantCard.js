5a73e46ddd708e967143689c7f133fc33c49c954
anthonysilva98 <anthonyssilva98@gmail.com>
2023-02-19 11:49:55 -0800

    Restaurant styling

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -16,7 +16,7 @@ const RestaurantCard = ({
   lat,
 }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="bg-white mr-3 shadow">
       <Image source={{ uri: imgUrl }} className="h-36 w-64 rounded-sm" />
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2">{title}</Text>
@@ -25,6 +25,10 @@ const RestaurantCard = ({
           <Text className="text-sx text-gray-500">
             <Text className=" text-green-500">{rating} </Text> • {genre}
           </Text>
+        </View>
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="grey" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby • {address} </Text>
         </View>
       </View>
     </TouchableOpacity>
