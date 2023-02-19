3ef0c0452b8a2c7981200974b671a0530a0639a4
anthonysilva98 <anthonyssilva98@gmail.com>
2023-02-19 11:13:02 -0800

    Shows catagories available to be clicked on

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 
-const CategoryCard = () => {
+const CategoryCard = ({ imageUrl, title }) => {
   return (
-    <View>
-      <Text>CategoryCard</Text>
-    </View>
+    <TouchableOpacity className="mr-2 relative">
+      <Image source={{ uri: imageUrl }} className="h-20 w-20 rounded" />
+      <Text className="absolute bottom-1 left-1 font-bold text-white">
+        {title}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
